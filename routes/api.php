a5ab404425ef06d91973398d83d775707cf8eb63
Jim Darlington <jdarlington78298@gmail.com>
2025-05-27 14:06:28 +0100

    API routes file loaded in boootstrap.

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AppointmentController;
 
-Route::middleware('auth:sanctum')->get('appointments', [AppointmentController::class, 'apiIndex']);
+Route::get('/appointments', [AppointmentController::class, 'apiIndex']);
